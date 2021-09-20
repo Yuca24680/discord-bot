48a20261ceaf45d16ce00fe584244de79bc24544
Nicholas Wong <40312631+kozr@users.noreply.github.com>
2021-09-20 01:35:05 -0700

    Update commands/chaddicus.js

--- commands/chaddicus.js
+++ commands/chaddicus.js
@@ -2,9 +2,10 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('chaddicusSupremus')
-		.setDescription('Test command'),
+		.setName('chaddicus')
+		.setDescription('returns chaddius'),
 	async execute(interaction) {
-		await interaction.reply('https://raw.githubusercontent.com/Yuca24680/discord-bot/344d38f2f1cf6f1ae357ad4a870829ddbf51941f/assets/chad.png');
-	},
+    await interaction.reply({files: ['./assets/chad.png']});
+  },
 };
+
